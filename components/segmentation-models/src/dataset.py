846266d4be48c1048c18476cbc29304cf00c6aed
Antonio Zarauz Moreno <antoniozarauz@bcp.com.pe>
2022-12-27 15:44:34 +0100

    add segmentation-models component

---
+++ components/segmentation-models/src/dataset.py
@@ -0,0 +1,88 @@
+# Libraries
+import os
+import cv2
+import torch
+import albumentations as A
+from segmentation_models_pytorch.encoders import get_preprocessing_params
+from torch.utils.data import Dataset
+
+# Main class
+class ImageSegmentationDataset(Dataset):
+    """Image segmentation dataset."""
+
+    def __init__(self, path, idx_list, encoder_flag:str='timm-mobilenetv3_large_100', encoder_weights:str='imagenet', img_size:int=128, num_labels:int=19, is_train:bool=True):
+        """
+        Object that fetches images and masks.
+
+        Args:
+            root_dir (str): Root directory of the dataset containing the images + annotations.
+            feature_extractor (SegFormerFeatureExtractor): feature extractor to prepare images + segmentation maps.
+            is_train (bool): Whether to apply training augmentation or validation
+        """
+        super(ImageSegmentationDataset, self).__init__()
+        self.path = path
+        self.idx_list = idx_list
+        self.prep_params = get_preprocessing_params(encoder_flag, pretrained=encoder_weights)
+        self.img_size = img_size
+        self.num_labels = num_labels
+        self.is_train = is_train
+
+        self.img_dir = os.path.join(self.path, "images")
+        self.ann_dir = os.path.join(self.path, "annotations")
+        self.num_samples = len(self.idx_list)
+
+    def __len__(self):
+        return self.num_samples
+
+    def __getitem__(self, idx):
+        idx = self.idx_list[idx]
+        im, annot=str(idx).zfill(6)+'.png', str(idx).zfill(6)+'_seg.png'
+        image = cv2.imread(os.path.join(self.img_dir, im))
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        
+        segmentation_map = cv2.imread(os.path.join(self.ann_dir, annot))
+        segmentation_map = cv2.cvtColor(segmentation_map, cv2.COLOR_BGR2GRAY)
+
+        if self.is_train:
+            pipe = A.Compose([
+                A.Resize(height=self.img_size, width=self.img_size, interpolation=cv2.INTER_LINEAR, always_apply=True),
+                A.PadIfNeeded(min_height=self.img_size, min_width=self.img_size, always_apply=True, border_mode=cv2.BORDER_CONSTANT),
+                A.HorizontalFlip(p=0.5),       
+                A.OneOf([
+                    A.ElasticTransform(alpha=120, sigma=120 * 0.05, alpha_affine=120 * 0.03, p=0.5),
+                    A.GridDistortion(p=0.5),
+                    A.OpticalDistortion(distort_limit=.75, shift_limit=0.5, p=1)                  
+                    ], p=0.8),
+                A.CLAHE(clip_limit=2, tile_grid_size=(16,16), p=0.8),
+                A.RandomBrightnessContrast(brightness_limit=(-.1,.1), contrast_limit=(-.1,.1), p=0.8),    
+                A.RandomGamma(p=0.8)])
+            augmented = pipe(image=image, mask=segmentation_map)
+            encoded_inputs = self.preprocess_input(augmented['image'], **self.prep_params)
+        else:
+            pipe = A.Compose([
+                A.Resize(height=self.img_size, width=self.img_size, interpolation=cv2.INTER_LINEAR, always_apply=True),
+                A.PadIfNeeded(min_height=self.img_size, min_width=self.img_size, always_apply=True, border_mode=cv2.BORDER_CONSTANT)
+                ])
+            augmented = pipe(image=image, mask=segmentation_map)
+            encoded_inputs = self.preprocess_input(augmented['image'], **self.prep_params)
+        return (encoded_inputs.squeeze(), augmented['mask'].squeeze())
+
+    @staticmethod
+    def preprocess_input(x, mean=None, std=None, input_space="RGB", input_range=None, **kwargs):
+
+            if input_space == "BGR":
+                x = x[..., ::-1].copy()
+
+            if input_range is not None:
+                if x.max() > 1 and input_range[1] == 1:
+                    x = x / 255.0
+
+            if mean is not None:
+                mean = torch.Tensor(mean)
+                x = x - mean
+
+            if std is not None:
+                std = torch.Tensor(std)
+                x = x / std
+
+            return torch.permute(x, (0,3,1,2))
---
+++ components/segmentation-models/src/setup.py
@@ -0,0 +1,129 @@
+# Libraries
+import os
+import shutil
+from pathlib import Path
+import requests
+import zipfile
+import random
+import logging as log
+from tqdm import tqdm
+
+# Main method
+def setup_folder_structure_FTYM(
+    pathdir: str='input'
+):
+    """Prepare folder structurte with image dataset.
+
+    Args:
+        path (str, optional): Folder where dataset will be unfolded. Defaults to './input'.
+    """
+
+    if not os.path.isdir(os.path.join(pathdir, 'train', 'images')):
+        #
+        # PART I: Training data
+        #
+
+        # Create directory and change working one
+        Path(os.path.join(pathdir, 'train', 'images')).mkdir(parents=True, exist_ok=True)
+        Path(os.path.join(pathdir, 'train', 'annotations')).mkdir(parents=True, exist_ok=True)
+        # Fetch information and load
+        log.debug("Fetch dataset")
+        with requests.get("https://facesyntheticspubwedata.blob.core.windows.net/iccv-2021/dataset_1000.zip", allow_redirects=True) as r:
+            open(os.path.join(pathdir, 'train', 'FTYM.zip'), 'wb').write(r.content)
+        # Unzip file
+        with zipfile.ZipFile(os.path.join(pathdir, 'train', 'FTYM.zip'), 'r') as zipObj:
+            zipObj.extractall(pathdir)
+        # Remove .zip
+        os.remove(os.path.join(pathdir, 'train', 'FTYM.zip'))
+        # Split indices
+        log.debug("Split indices")
+        train_idx = random.sample(range(1000), 800)
+        val_idx = [x for x in range(1000) if x not in train_idx]
+        # Create folders and accomodate selected images-annotations
+        log.debug("Training data")
+        for idx in tqdm(train_idx):
+            # Format idx as it is written in filenames
+            filename_image=str(idx).zfill(6)+'.png'
+            filename_mask=str(idx).zfill(6)+'_seg.png'
+            filename_anot=str(idx).zfill(6)+'_ldmks.txt'
+            # Move image
+            os.replace(os.path.join(pathdir, filename_image), os.path.join(pathdir, 'train', 'images', filename_image))
+            # Move segmentation mask
+            os.replace(os.path.join(pathdir, filename_mask), os.path.join(pathdir, 'train', 'annotations', filename_mask))
+            # Drop landmark annotation
+            os.remove(os.path.join(pathdir, filename_anot))
+
+
+        #
+        # PART II: Validation data
+        #
+
+        # Create directory and change working one
+        Path(os.path.join(pathdir, 'val', 'images')).mkdir(parents=True, exist_ok=True)
+        Path(os.path.join(pathdir, 'val', 'annotations')).mkdir(parents=True, exist_ok=True)
+        # Create folders and accomodate selected images-annotations
+        log.debug("Validation data")
+        for idx in tqdm(val_idx):
+            # Format idx as it is written in filenames
+            filename_image=str(idx).zfill(6)+'.png'
+            filename_mask=str(idx).zfill(6)+'_seg.png'
+            filename_anot=str(idx).zfill(6)+'_ldmks.txt'
+            # Move image
+            os.replace(os.path.join(pathdir, filename_image), os.path.join(pathdir, 'val', 'images', filename_image))
+            # Move segmentation mask
+            os.replace(os.path.join(pathdir, filename_mask), os.path.join(pathdir, 'val', 'annotations', filename_mask))
+            # Drop landmark annotation
+            os.remove(os.path.join(pathdir, filename_anot))
+    else:
+        train_idx = [int(x.split('.')[0].lstrip('0')) if x!='000000.png' else 0 for x in os.listdir(os.path.join(pathdir, 'train', 'images'))]
+        val_idx = [int(x.split('.')[0].lstrip('0')) if x!='000000.png' else 0 for x in os.listdir(os.path.join(pathdir, 'val', 'images'))]
+    
+    #
+    # Part III: Labels mapping and homemade stuff
+    #
+
+    tag2idx = {
+        'BACKGROUND' : 0,
+        'SKIN' : 1,
+        'NOSE' : 2,
+        'RIGHT_EYE' : 3,
+        'LEFT_EYE' : 4,
+        'RIGHT_BROW' : 5,
+        'LEFT_BROW' : 6,
+        'RIGHT_EAR' : 7,
+        'LEFT_EAR' : 8,
+        'MOUTH_INTERIOR' : 9,
+        'TOP_LIP' : 10,
+        'BOTTOM_LIP' : 11,
+        'NECK' : 12,
+        'HAIR' : 13,
+        'BEARD' : 14,
+        'CLOTHING' : 15,
+        'GLASSES' : 16,
+        'HEADWEAR' : 17,
+        'FACEWEAR' : 18
+    }
+
+    class_weights = [
+        0.0001658747314334,
+        0.0012043172954295,
+        0.0137276061200101,
+        0.1968507907700739,
+        0.1964319631162028,
+        0.0942255875320043,
+        0.0933697862761819,
+        0.0547495411674153,
+        0.0535336807499800,
+        0.0951002747093535,
+        0.0565119300908439,
+        0.0493595760815996,
+        0.0062370452717332,
+        0.0012254438559717,
+        0.0154623923498118,
+        0.0007041252515757,
+        0.0630263970748995,
+        0.0081136675554797,
+        0.0000000000000000
+    ]
+
+    return train_idx, val_idx, tag2idx, class_weights
